f57d619377eeefbfc96b5d236e738270edb64b69
Hao Li <50431940+LIMUXUALE0927@users.noreply.github.com>
2024-04-09 06:36:06 +0000

    update

--- Hello.py
+++ Hello.py
@@ -13,39 +13,36 @@
 # limitations under the License.
 
 import streamlit as st
-from streamlit.logger import get_logger
-
-LOGGER = get_logger(__name__)
+from streamlit_gsheets import GSheetsConnection
 
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+st.set_page_config(
+    page_title="IS5451 Group06",
+    page_icon="👋",
+)
 
-    st.write("# Welcome to Streamlit! 👋")
+st.write("# IS5451 Group06 Final Project 👋")
 
-    st.sidebar.success("Select a demo above.")
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+st.markdown(
     """
-    )
+    Streamlit is an open-source app framework built specifically for
+    Machine Learning and Data Science projects.
+    **👈 Select a demo from the sidebar** to see some examples
+    of what Streamlit can do!
+    ### Want to learn more?
+    - Check out [streamlit.io](https://streamlit.io)
+    - Jump into our [documentation](https://docs.streamlit.io)
+    - Ask a question in our [community
+      forums](https://discuss.streamlit.io)
+"""
+)
 
+# Create a connection object.
+conn = st.connection("gsheets", type=GSheetsConnection)
 
-if __name__ == "__main__":
-    run()
+df = conn.read()
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
